3c4ce9c991483ae14e5831ce137bafe34bb7d7a6
Dimitris Alikaniotis <dim.alikaniotis@gmail.com>
2017-02-16 01:35:14 +0000

    minor changes
    split the utils.R to utils (containing text reading and preprocessing
    functions) and cache_functions which interfaces with the cache
    environment. Also wrote quite a bit of documentation for the exposed
    functions.

--- R/utils.R
+++ R/utils.R
@@ -1,24 +1,41 @@
-StoreInCache <- function(key, value, CacheEnv) {
-    ## Store a value in cache.
-    ##
-    ## Args:
-    ##   key (character): The key to later retrieve the value.
-    ##   value (object): An arbitrary object to be stored.
-    ##   CacheEnv (environment): The environment to store the key into.
-    if (is.numeric(key))
-        key <- as.character(key)
-    assign(key, value, envir = CacheEnv)
+#' Preprocess character vector
+#'
+#' This provides some elementary preprocessing for a read character
+#' vector such as lowercasing and normalization. The normalization
+#' step substitutes each element of the vector with a numeric value
+#' (its ID). This can be quite useful in non-ASCII texts or texts
+#' containing words with boudnary symbols where the regular expression
+#' can fail.
+#'
+#' @param text
+#' @param lower This happens before normalization.
+#' @param normalize
+#'
+#' @return A character vector.
+#'
+#' @examples
+#' txt <- c("This", "is", "a", "Sentence", "containing", "UPPERCASE", "lowercase", "and", "sy.mb'ols")
+#' txt.norm <- NormalizeText(txt, lower = TRUE, normalize = TRUE)
+
+NormalizeText <- function(text, lower = FALSE, normalize = TRUE) {
+    if (lower)
+        text <- tolower(text)
+    if (normalize)
+        text <- as.character(as.numeric(as.factor(text)))
+    return(text)
 }
 
-read.file <- function(text.file, max.length = length(text.file)) {
-    return(scan(text.file, what = "char", quote = "",
+read.file <- function(text.file, max.length = length(text.file),
+                      lower = FALSE, normalize = FALSE) {
+   text <- scan(text.file, what = "char", quote = "",
                 comment.char = "", encoding = "UTF-8",
-                sep = " ", n = max.length))
+                sep = " ", n = max.length)
+   return(NormalizeText(text, lower = lower, normalize = normalize))
 }
    
 IsSubstring <- function(subvec, fullstr) {
-    ## Tests whether `subvec` is a proper substring of `fullvec`.
-    ##
+    #' Tests whether `subvec` is a proper substring of `fullvec`.
+    #'
     ## Args:
     ##   subvec: A character vector of size less than `fullvec`.
     ##   fullstr: A character vector.
@@ -28,32 +45,4 @@ IsSubstring <- function(subvec, fullstr) {
     ## Transform to regex pattern
     substr  <- paste0('\\b', paste(subvec, collapse=' '), '\\b')
     return (grepl(pattern = substr, x = fullstr))
-}
-
-GetFromCache <-function(key, CacheEnv) {
-    ## Retrieve value from cache
-    ##
-    ## Args:
-    ##   key (character): The key to retrieve.
-    ##   CacheEnv (environment): The environment to retrieve the key from.
-    ##
-    ## Returns:
-    ##   object: An arbitrary object stored in the environment.
-    if (is.numeric(key))
-        key <- as.character(key)
-    return (get(key, envir = CacheEnv, inherits = FALSE))
-}
-
-ExistsInCache <- function(key, CacheEnv) {
-    ## Check whether key exists in cache.
-    ##
-    ## Args:
-    ##   key (character): The key to retrieve.
-    ##   CacheEnv (environment): The environment to retrieve the key from.
-    ##
-    ## Returns:
-    ##   bool: `TRUE` if `key` exists in `CacheEnv` else `FALSE`.
-    if (is.numeric(key))
-        key <- as.character(key)
-    return (exists(key, envir = CacheEnv, inherits = FALSE))
 }
