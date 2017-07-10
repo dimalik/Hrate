f930d1c6dff2d4894ed78933c89f8afe8c591ced
Dimitris Alikaniotis <dim.alikaniotis@gmail.com>
2017-07-10 03:26:38 +0100

    tests on stabilize estimate

--- tests/testthat/test.stabilize.estimate.R
+++ tests/testthat/test.stabilize.estimate.R
@@ -19,3 +19,14 @@ test_that("get.estimate works properly", {
     expect_equal(get.estimate(deuparl, max.length = 20000, every.word = 100), GERMAN.20000.100, tolerance=1e-4)
     expect_equal(get.estimate(deuparl, max.length = 20000, every.word = 1000), GERMAN.20000.1000, tolerance=1e-4)    
 })
+
+
+dd <- data.frame(Corpus.Size = c(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000),
+                 Entropy = c(6.27897014441326, 6.41380694727928, 6.32359644277194, 6.59773465512657, 6.63737801766681, 6.74321247576949, 6.6668246644019, 6.72546530332448, 6.79647495155925, 6.84333087495644))
+
+se <- stabilize.estimate(deuparl, max.length = 10000, step.size = 1000, verbose = FALSE)
+
+test_that("stabilize.estimate works fine", {
+    expect_is(se, "StabilizeEntropy")
+    expect_equal(get.stabilization(se), dd)
+})
